59036376ad672134cf524c73b7e643a7cfc34933
stouma915 <sasakitouma@yahoo.ne.jp>
2022-04-01 10:52:49 +0900

    HTTPステータスコードを送信する

--- src/status_checker.rs
+++ src/status_checker.rs
@@ -9,10 +9,19 @@ pub async fn send_status(http: &Arc<Http>, channel: &ChannelId) -> () {
     let client = reqwest::Client::new();
 
     match client.get(&*CONFIG.target_url).send().await {
-        Ok(response) => channel
-            .say(http, ":white_check_mark: Connected.")
-            .await
-            .ok(),
+        Ok(response) => {
+            channel
+                .say(http, ":white_check_mark: Connected.")
+                .await
+                .ok();
+
+            let message = if response.status().is_success() {
+                format!(":white_check_mark: {}.", response.status().to_string())
+            } else {
+                format!(":x: {}.", response.status().to_string())
+            };
+            channel.say(http, message).await.ok()
+        }
         Err(err) => {
             let message = format!(":x: Failed to connect:\n{:?}", err);
             channel.say(http, message).await.ok()
